7f279bb819664bd9f07d310d93d35dfca68667d6
Mancii1 <lungelomanci15@gmail.com>
2025-03-13 00:36:37 +0200

    Further Backend Development

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
- 
+from flask import Flask, render_template, redirect, url_for, flash
+from config import Config
+from models import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def home():
+    return render_template('dashboard.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
